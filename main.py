206b49a8e182291d22834989bc5d4bb80bb2f44f
Kirill Sayed <sayed.kirill@gmail.com>
2021-11-27 15:35:01 +0300

    Added main loop

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import pygame
+
+
+class Game:
+    """Main class of the game"""
+
+    def __init__(self, size=(1280, 640), screen=None):
+        self.size = size
+        self.screen = screen if screen is not None else pygame.display.set_mode(self.size)
+        self.is_game_over = False
+
+    def run(self) -> None:
+        """
+        Main loop
+
+        :return: None
+        """
+
+        pygame.init()
+        while not self.is_game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_game_over = True
+
+
 def main():
-    pass
+    game = Game()
+    game.run()
 
 
 if __name__ == '__main__':
